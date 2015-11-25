d320e955e41a5f25167398ff03b454b5364b0f06
Ben Hur Bahia do Nascimento <benhur@tbit.com.br>
2015-11-25 17:17:30 -0200

    distancia euclidiana implementado

--- GitTests/Point.cs
+++ GitTests/Point.cs
@@ -1,8 +1,13 @@
-﻿namespace GitTests
+﻿using static System.Math;
+
+namespace GitTests
 {
     public class Point
     {
         public int x { get; }
         public int y { get; }
+
+        public double Distance(Point other)
+            => Sqrt(Pow(this.x - other.x, 2) + Pow(this.y - other.y, 2));
     }
 }
